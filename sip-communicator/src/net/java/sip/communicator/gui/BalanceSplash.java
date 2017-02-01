78d5985de52e8acccb16ea292308d54ce651b81a
Marialena <Marialena@147.102.236.149>
2017-02-01 20:00:57 +0200

    Finalised

--- sip-communicator/src/net/java/sip/communicator/gui/BalanceSplash.java
+++ sip-communicator/src/net/java/sip/communicator/gui/BalanceSplash.java
@@ -1,5 +1,6 @@
 package net.java.sip.communicator.gui;
 
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.ParseException;
@@ -11,17 +12,19 @@ import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 import net.java.sip.communicator.SipCommunicator;
 import net.java.sip.communicator.sip.CommunicationsException;
 import net.java.sip.communicator.sip.RequestProcessing;
 
 public class BalanceSplash {
-	
+	private static JTextArea textArea = new JTextArea(1, 5);
 	private static JFrame frame;
     private static String userBalance;
     static JComboBox<String> cb;
-    
+    private static JPanel mainPanel = new JPanel();
+   
     public static void ShowBalance(int width, int height){
         RequestProcessing requestProcessing = new RequestProcessing();
 
@@ -42,26 +45,42 @@ public class BalanceSplash {
         }
 
         System.out.println("DEBUG: out of the loop!");
-
+        
         userBalance = SipCommunicator.globalBalance;
-        JPanel panel = new JPanel();
-        JButton user = new JButton(userBalance);
         JButton applyButton = new JButton("Apply");
         JButton cancelButton = new JButton("Cancel");
-
+        applyButton.setEnabled(true);
+        cancelButton.setEnabled(true);
+        
+        JPanel nested1 = new JPanel(); // FlowLayout
+        JLabel lbl1 = new JLabel("Would you like to add to your balance?");
+        lbl1.setVisible(true);
+        nested1.add(lbl1);
+        JPanel nested2 = new JPanel(); // 
+        JLabel lbl3 = new JLabel("Current Balance");
+        nested2.add(lbl3);
+        nested2.add(new JLabel(userBalance));
+        JPanel nested3 = new JPanel(); // FlowLayout
+        nested3.add(new JLabel("Type your number"));
+        nested3.add(textArea);  
+        JPanel nested4 = new JPanel(); // FlowLayout
+        nested4.add(applyButton);
+        nested4.add(cancelButton);
+        JPanel outer = new JPanel();
+        outer.add(nested1);
+        outer.add(nested2);
+        outer.add(nested3);
+        outer.add(nested4);
+        mainPanel = outer;
+        
+        
         JFrame localframe = new JFrame();
         frame = localframe;
         frame.setVisible(true);
         frame.setSize(width, height);
         frame.setLocation(200, 100);
-        frame.add(panel);
+        frame.add(mainPanel);
 
-        JLabel lbl = new JLabel("Would you like to cancel the forwarding to this user ?");
-        lbl.setVisible(true);
-
-        panel.add(lbl);
-        applyButton.setEnabled(true);
-        cancelButton.setEnabled(true);
 
         ActionListener applyListener = new ActionListener()
         {
@@ -79,24 +98,26 @@ public class BalanceSplash {
                 cancelBalanceButton_actionPerformed(evt);
             }
         };
-        cancelButton.addActionListener(cancelListener);
-        panel.add(user);
-        panel.add(applyButton);
-        panel.add(cancelButton);
-
+        cancelButton.addActionListener(cancelListener);       
     }
     static void applyBalanceButton_actionPerformed(EventObject evt) {
-        String amount = (String) cb.getSelectedItem();
+    	String amount = textArea.getText().trim();
+        textArea.setText(null);
         System.out.println("I WILL ADD " + amount + " DOLLARS");
 
-        RequestProcessing requestProcessing = new RequestProcessing();
+        try{
+        	double d = Double.parseDouble(amount);
+        	RequestProcessing requestProcessing = new RequestProcessing();
 
-        try {
-            requestProcessing.processInfo("BALANCE", amount);
-        } catch (Exception exc) {
-            return;
+        	try {
+        		requestProcessing.processInfo("BALANCE", amount);
+        	} catch (Exception exc) {
+        		return;
+        	}
         }
-
+        catch(NumberFormatException nfe){
+        	return;
+        }
         SipCommunicator.globalBalance = null;
         frame.dispose();
     }
@@ -107,3 +128,4 @@ public class BalanceSplash {
     }	
 
 }
+
